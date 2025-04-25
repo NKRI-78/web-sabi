bf61f4c0d861c33798a0b96096692a2337b60db9
Reihan Agam <reihanagam7@gmail.com>
2025-04-25 16:37:12 +0700

    update

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -38,11 +38,21 @@ const Header: React.FC = () => {
                 <div className="w-full my-4 max-w-xl relative">
                     <input
                         onChange={(e) => dispatch(setSearch(e.target.value))}
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter") {
+                              e.preventDefault();
+                              onSubmit();
+                            }
+                        }}
                         type="text"
                         placeholder="Search..."
                         className="w-full px-4 py-2 pr-10 text-black rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 shadow-sm"
                     />
-                    <Search onClick={onSubmit} className="absolute cursor-pointer right-3 top-1/2 transform -translate-y-1/2 text-gray-500" size={20} />
+                    
+                    <Search onSubmit={(e) => {
+                        e.preventDefault();
+                        onSubmit();
+                    }} onClick={() => onSubmit()}  className="absolute cursor-pointer right-3 top-1/2 transform -translate-y-1/2 text-gray-500" size={20} />
                 </div>
             </header>
         </div>
