6d0a96b56a3483249d9895247670548577cdbaad
Reihan Agam <reihanagam7@gmail.com>
2025-07-21 12:59:17 +0700

    update

--- app/components/content/ContentKendaraan.tsx
+++ app/components/content/ContentKendaraan.tsx
@@ -107,6 +107,14 @@ const ContentKendaraan: React.FC = () => {
                       {item.BPKB ?? "N/A"}
                     </span>
                   </div>
+                  <div className="flex items-center gap-2 text-gray-600 text-sm mt-2">
+                    <strong className="truncate overflow-hidden whitespace-nowrap max-w-full block">
+                      No Polisi :
+                    </strong>
+                    <span className="truncate overflow-hidden whitespace-nowrap max-w-full block">
+                      {item.AutoNumber ?? "N/A"}
+                    </span>
+                  </div>
                   <div
                     className="flex items-center gap-2 text-gray-600 text-sm mt-2"
                     // onClick={
